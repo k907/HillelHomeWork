121cf8c47bb8310999a6784ab0a1897c0476b1f8
Aleksey <7019405@gmail.com>
2020-07-24 17:44:57 +0300

    Home work 24/07/2020 lessons 7

---
+++ src/Lessons_7/MyColection.java
@@ -0,0 +1,101 @@
+package Lessons_7;
+
+import java.util.Arrays;
+
+/*
+Реализовать простую коллекцию для String на базе массива.
+методы:
+добавлять (add), (по индеку или значению)
+удалять(delete), (по индексу или значению)
+доставать по индексу (get),
+*/
+public class MyColection {
+
+    private int size;
+    private String[] arrayData = new String[10];
+
+    // Добавление в конец массива
+    public void add ( String value ) {
+
+        // увеличим массив если нужно
+        ensureCapacity(size + 1 );
+        arrayData[size++] = value;
+    }
+
+    // Добавление по индексу
+    public void add ( int index, String value ) throws ArrayIndexOutOfBoundsException {
+
+        if (index < 0 || index > size - 1 ) {
+            throw new ArrayIndexOutOfBoundsException ("Некорректный индекс!");
+        }
+
+        // увеличим массив если нужно
+        ensureCapacity(size + 1 );
+        arrayСopy(arrayData.clone(), index, arrayData, index + 1, size-index);
+
+        arrayData[index] = value;
+        size++;
+    }
+
+    // Удаление по индексу
+    public void remove ( int index ) throws ArrayIndexOutOfBoundsException {
+
+        if (index < 0 || index > size - 1 ) {
+            throw new ArrayIndexOutOfBoundsException ("Некорректный индекс!");
+        }
+
+        arrayСopy(arrayData.clone(), index+1, arrayData, index,  size - index - 1);
+        arrayData[--size] = null;
+    }
+
+    // Удаление по значению
+    public void remove ( String value ) {
+
+            for (int i = 0; i < size; i++) {
+
+                 if ( arrayData[i].equals(value) ) {
+                     remove (i);
+                 }
+            }
+    }
+
+    @Override
+    public String toString() {
+        return "MyColection{" +
+                "size=" + size +
+                ", arrayData=" + Arrays.toString(arrayData) +
+                '}';
+    }
+
+
+    // Увеличивает массив если закончились свободные элементы
+    private void ensureCapacity ( int minCapacity ) {
+
+        if ( minCapacity > arrayData.length ) {
+            String[] oldData = arrayData;
+            arrayData = new String[(oldData.length*3)/2+1];
+            arrayСopy(oldData, 0, arrayData, 0 , oldData.length );
+        }
+    }
+
+    /**
+     * Копирует элементы в новый массив
+     * @param from - массив, который копируем
+     * @param fromIndex индекс в массиве from начиная с которого берем элементы для копирования
+     * @param to - массив, который копируем
+     * @param toIndex - индекс в массиве to начиная с которого вставляем элементы
+     * @param count - количество элементов которые берем из массива from и вставляем в массив to
+     */
+    private void arrayСopy(String[] from, int fromIndex, String[] to, int toIndex, int count ) throws IllegalArgumentException
+    {
+
+        if ( to.length  < (toIndex + count) || from.length < (fromIndex + count)  ) {
+            throw new IllegalArgumentException("Длина массива меньше минимально значения");
+        }
+
+        for( int i = fromIndex; i < (fromIndex + count); i++  ) {
+            to[toIndex++] = from[i];
+        }
+    }
+
+}
---
+++ src/Lessons_7/Run.java
@@ -0,0 +1,40 @@
+package Lessons_7;
+
+import java.util.Arrays;
+
+public class Run {
+
+  public static void main(String[] args) {
+
+    MyColection colection = new MyColection();
+
+      colection.add("0");
+      colection.add("1");
+      colection.add("2");
+      colection.add("3");
+      colection.add("4");
+      colection.add("5");
+
+     System.out.println(colection.toString());
+
+    // добавим элемент в конец массива
+    colection.add("6");
+    System.out.println(colection.toString());
+
+    // добавим элемент по индексу
+     colection.add(20, "new_val");
+     System.out.println(colection.toString());
+
+    // удалим элемент по индексу
+     colection.remove (1);
+     System.out.println(colection.toString());
+
+    // удалим элемент по значению
+    colection.remove ("3");
+    System.out.println(colection.toString());
+
+  }
+
+
+}
+
